5ee15de12d935956f8854a6ebc0dbd9c79e2b0a8
Abhinav <abhinavnyps@gmail.com>
2026-02-26 21:00:41 +0530

    Add solution for average of even numbers divisible by three
    Implement a solution to calculate the average of even numbers divisible by three.

---
+++ Day4/2455.AverageValueofEvenNumbersThatAreDivisiblebyThree.py
@@ -0,0 +1,13 @@
+class Solution:
+    def averageValue(self, nums: List[int]) -> int:
+        count=0
+        sum=0
+        for i in nums:
+            if i%6==0:
+                count+=1
+                sum+=i
+        if count==0:
+            return 0
+        else:
+            return sum//count
+        
