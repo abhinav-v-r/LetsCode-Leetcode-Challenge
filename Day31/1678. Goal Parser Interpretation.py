45637d82be551a7531c22322ac360041e163c35e
Abhinav <abhinavnyps@gmail.com>
2026-03-26 07:52:13 +0530

    Delete Day31/1678. Goal Parser Interpretation.py

--- Day31/1678. Goal Parser Interpretation.py
+++
@@ -1,6 +0,0 @@
-class Solution:
-    def interpret(self, command: str) -> str:
-        command=command.replace('()','o')
-        command=command.replace('(al)','al')
-        return command
-        
