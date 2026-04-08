c31fa41e0d612d38273a907e5cdeba71e865991e
Abhinav <abhinavnyps@gmail.com>
2026-04-08 12:58:14 +0530

    Create 2859. Sum of Values at Indices With K Set Bits.py

---
+++ Day45/2859. Sum of Values at Indices With K Set Bits.py
@@ -0,0 +1,9 @@
+class Solution:
+    def sumIndicesWithKSetBits(self, nums: List[int], k: int) -> int:
+        sum=0
+        for i in range(len(nums)):
+            if bin(i).count('1')==k:
+                sum+=nums[i]
+        return sum
+                
+        
